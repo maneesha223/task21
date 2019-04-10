2610b7937fa7d217ae3d5de2d974036c5ce6c219
mahitha <mahitha.reddy132@gmail.com>
2019-04-10 13:02:14 +0530

    calci

---
+++ calci.java
@@ -0,0 +1,24 @@
+package task21;
+
+public class calci {
+
+	public double Add(double num1,double num2)
+	{
+	return num1+num2;
+	}
+	public double Mul(double number1,double number2)
+	{
+	return number1*number2;
+	}
+	public double Div(double number1,double number2)
+	{
+		try
+		{
+			return (number1/number2);
+		}
+		catch(Exception e)
+		{
+			return Integer.parseInt(e.getMessage());
+		}
+	}
+}
